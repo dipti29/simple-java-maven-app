06657bb7931b1fb6af856342736a17679e4d0dc5
dipti29 <62116208+dipti29@users.noreply.github.com>
2020-03-13 11:50:59 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,16 +6,10 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
-
-    public App() {}
+   
 
     public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
-
-    private final String getMessage() {
-        return message;
+        System.out.println("Hello Dipti!!!!!!");
     }
 
 }
